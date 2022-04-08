0324e47b8626c4080ef9b6fa06749d3c0f29caab
Dichlorobenzene <814841350@qq.com>
2022-04-08 11:04:12 +0800

    Redirect after POST, and show all items in template

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 # Create your views here.
 def home_page(request):
-   return render(request,'home.html',{
-      'new_item_text' : request.POST.get('item_text','')
-   })
+   # item = Item()
+   # item.text = request.POST.get('item_text','')
+   # item.save()
+
+   # return render(request,'home.html',{
+   #    'new_item_text' : item.text
+   # })
+   if request.method == 'POST':
+      # new_item_text = request.POST['item_text']
+      Item.objects.create(text=request.POST['item_text'])
+      return redirect('/')
+   else:
+      new_item_text=''
+   items = Item.objects.all()
+   return render(request,'home.html',{'items':items})
