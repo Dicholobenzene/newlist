55e85cff348b3112ad5342aa90890efa90b246c1
Dichlorobenzene <814841350@qq.com>
2022-04-08 10:05:48 +0800

    add helper

--- test.py
+++ test.py
@@ -6,9 +6,13 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    # def tearDown(self):
-    #     self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
         
@@ -32,19 +36,19 @@ class NewVisitorTest(unittest.TestCase):
         # #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
         # # )
         # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
