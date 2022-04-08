e36c7b3c6b533a8d1f1f40e9d66deb62bf118825
Dichlorobenzene <814841350@qq.com>
2022-04-08 09:48:49 +0800

    do false post

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,25 +2,36 @@ from urllib import request, response
 from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page
+from views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.views import home_page
+from views import home_page
 from django.shortcuts import render
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func,home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func,home_page)
 
-    def test_home_page_returns_correct_html(self):
-        # request = HttpRequest()
-        # response = home_page(request)
+    # def test_home_page_returns_correct_html(self):
+    #     # request = HttpRequest()
+    #     # response = home_page(request)
+    #     response = self.client.get('/')
+
+    #     html = response.content.decode('utf8')
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>,html')
+    #     self.assertTrue(html.strip().endswith('<html>'))
+
+    #     self.assertTemplateUsed(response,'home.html')
+
+    def test_uses_home_template(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>,html')
-        self.assertTrue(html.strip().endswith('<html>'))
-
         self.assertTemplateUsed(response,'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',data = {'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
+
+    
