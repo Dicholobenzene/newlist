abf0349851c4c9854b425fddd40c916962fbd93e
Dichlorobenzene <814841350@qq.com>
2022-05-30 11:17:37 +0800

    new html

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,18 +3,11 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 # Create your views here.
 def home_page(request):
-   # item = Item()
-   # item.text = request.POST.get('item_text','')
-   # item.save()
-
-   # return render(request,'home.html',{
-   #    'new_item_text' : item.text
-   # })
    if request.method == 'POST':
-      # new_item_text = request.POST['item_text']
       Item.objects.create(text=request.POST['item_text'])
-      return redirect('/')
-   else:
-      new_item_text=''
+      return redirect('/lists/the-only-list-in-the-world')
+   return render(request,'home.html')
+
+def view_list(request):
    items = Item.objects.all()
-   return render(request,'home.html',{'items':items})
+   return render(request,'list.html',{'items':items})
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -2,5 +2,6 @@ from lists import views
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'^$',views.home_page,name='home')
+    re_path(r'^$',views.home_page,name='home'),
+    re_path(r'^lists/the-only-list-in-the-world/$',views.view_list,name='view_list')
 ]
